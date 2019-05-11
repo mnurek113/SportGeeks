c8e5d99c671d9c7dbb6dd00c0e664df9144a4465
Michał Krzyżanek <mikrzyzanek@gmail.com>
2019-05-11 20:12:24 +0200

    Solved problem by changing minsdk to 21

--- app/src/main/java/com/code/knab/sportgeeks/EventsActivity.java
+++ app/src/main/java/com/code/knab/sportgeeks/EventsActivity.java
@@ -1,6 +1,6 @@
 package com.code.knab.sportgeeks;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EventsActivity extends AppCompatActivity {
