2488c22d0dc32c673c8187256fcfdec68a8527e2
Michał Krzyżanek <mikrzyzanek@gmail.com>
2019-05-12 01:26:59 +0200

    map in progress

---
+++ app/src/main/java/com/code/knab/sportgeeks/EventsSearchPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.code.knab.sportgeeks;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventsSearchPagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> eventsSearchFragmentList = new ArrayList<>();
+    private final List<String> eventsSearchFragmentTitleList = new ArrayList<>();
+
+
+    public EventsSearchPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    private void addFragment(Fragment fragment, String title) {
+        eventsSearchFragmentList.add(fragment);
+        eventsSearchFragmentTitleList.add(title);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return eventsSearchFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return eventsSearchFragmentList.size();
+    }
+}
