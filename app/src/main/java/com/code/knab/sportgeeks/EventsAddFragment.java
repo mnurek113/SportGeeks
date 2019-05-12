f14b8b812d5a648cf2e8f0789a88d6398406f4d1
jeyjey626 <juliamakulec@gmail.com>
2019-05-12 05:39:09 +0200

    events views develop

--- app/src/main/java/com/code/knab/sportgeeks/EventsAddFragment.java
+++ app/src/main/java/com/code/knab/sportgeeks/EventsAddFragment.java
@@ -1,16 +1,32 @@
 package com.code.knab.sportgeeks;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+
 
 public class EventsAddFragment extends Fragment {
 
+    private MaterialButton add_event_final_button;
+    private MaterialButton add_timePicker_button, add_datePicker_button;
+    private MaterialButton add_timePicker_end_button, add_datePicker_end_button;
+    private TextInputEditText add_in_start_date, add_in_end_date, add_in_start_time, add_in_end_rime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
 
     @Nullable
@@ -19,8 +35,75 @@ public class EventsAddFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_events_add,container,false);
 
 
-
-
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        MaterialButton add_datePicker_button = (MaterialButton) getView().findViewById(R.id.add_datePicker_button);
+        MaterialButton add_timePicker_button = (MaterialButton) getView().findViewById(R.id.add_timePicker_button);
+        MaterialButton add_datePicker_end_button = (MaterialButton) getView().findViewById(R.id.add_datePicker_end_button);
+        MaterialButton add_timePicker_end_button = (MaterialButton) getView().findViewById(R.id.add_timePicker_end_button);
+        final TextInputEditText add_in_start_date = (TextInputEditText) getView().findViewById(R.id.add_in_start_date);
+        TextInputEditText add_in_end_date = (TextInputEditText) getView().findViewById(R.id.add_in_end_date);
+        TextInputEditText add_in_start_time = (TextInputEditText) getView().findViewById(R.id.add_in_start_time);
+        TextInputEditText add_in_end_time = (TextInputEditText) getView().findViewById(R.id.add_in_end_time);
+
+
+
+        add_datePicker_button.setOnClickListener( v -> {
+
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (view1, year, monthOfYear, dayOfMonth) -> add_in_start_date
+                            .setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year), mYear, mMonth, mDay);
+            datePickerDialog.show();
+
+        });
+
+        add_datePicker_end_button.setOnClickListener( v -> {
+
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (view1, year, monthOfYear, dayOfMonth) -> add_in_end_date
+                            .setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year), mYear, mMonth, mDay);
+            datePickerDialog.show();
+
+        });
+
+        add_timePicker_button.setOnClickListener(v -> {
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
+                    (view13, hourOfDay, minute) -> add_in_start_time.
+                            setText(hourOfDay + ":" + minute), mHour, mMinute, true);
+            timePickerDialog.show();
+        });
+
+        add_timePicker_end_button.setOnClickListener(v -> {
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),
+                    (view12, hourOfDay, minute) -> add_in_end_time.
+                            setText(hourOfDay + ":" + minute), mHour, mMinute, true);
+            timePickerDialog.show();
+        });
+
     }
 }
--- app/src/main/java/com/code/knab/sportgeeks/ui/events/EventsActivity.java
+++ app/src/main/java/com/code/knab/sportgeeks/ui/events/EventsActivity.java
@@ -1,15 +1,48 @@
 package com.code.knab.sportgeeks.ui.events;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import com.code.knab.sportgeeks.EventsActiveFragment;
+import com.code.knab.sportgeeks.EventsAddFragment;
+import com.code.knab.sportgeeks.EventsSearchFragment;
+import com.code.knab.sportgeeks.EventsSearchPagerAdapter;
 import com.code.knab.sportgeeks.R;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.Calendar;
+
 
 public class EventsActivity extends AppCompatActivity {
+
+    private EventsSearchPagerAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        mViewPager = (ViewPager) findViewById(R.id.eventsPager);
+        setupViewPager(mViewPager);
+
+
     }
+
+    private void setupViewPager(ViewPager mViewPager) {
+        mSectionsPageAdapter = new EventsSearchPagerAdapter(getSupportFragmentManager());
+        mSectionsPageAdapter.addFragment(new EventsActiveFragment(), "EventsActiveFragment");
+        mSectionsPageAdapter.addFragment(new EventsSearchFragment(), "EventsSearchFragment");
+        mSectionsPageAdapter.addFragment(new EventsAddFragment(), "EventsAddFragment");
+        mViewPager.setAdapter(mSectionsPageAdapter);
+
+    }
+
+
 }
