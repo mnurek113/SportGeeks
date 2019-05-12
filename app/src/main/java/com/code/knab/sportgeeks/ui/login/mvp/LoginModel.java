f90adb0a159b7a5b57e006b1393ce14ecc5f1f97
Michał Jan Nurek <m.nurek@stud.elka.pw.edu.pl>
2019-05-12 04:55:13 +0200

    LoginActivity working

--- app/src/main/java/com/code/knab/sportgeeks/ui/login/mvp/LoginModel.java
+++ app/src/main/java/com/code/knab/sportgeeks/ui/login/mvp/LoginModel.java
@@ -1,4 +1,18 @@
 package com.code.knab.sportgeeks.ui.login.mvp;
 
-public class LoginModel {
+import com.code.knab.sportgeeks.network.api.NetworkApiProvider;
+import com.code.knab.sportgeeks.network.api.SignInApi;
+import com.code.knab.sportgeeks.network.json.User;
+
+import io.reactivex.Single;
+
+public class LoginModel implements LoginMVP.Model{
+
+    private NetworkApiProvider networkApiProvider = new NetworkApiProvider();
+    private SignInApi api = networkApiProvider.createSignInApi();
+
+    @Override
+    public Single<User> getUser(String userUID) {
+        return api.getUser(userUID);
+    }
 }
--- app/src/main/java/com/code/knab/sportgeeks/ui/login/mvp/LoginPresenter.java
+++ app/src/main/java/com/code/knab/sportgeeks/ui/login/mvp/LoginPresenter.java
@@ -1,4 +1,62 @@
 package com.code.knab.sportgeeks.ui.login.mvp;
 
-public class LoginPresenter {
+import com.code.knab.sportgeeks.network.json.User;
+import com.code.knab.sportgeeks.utils.SchedulerHolders;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executors;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class LoginPresenter implements LoginMVP.Presenter{
+
+    private static final boolean SIGN_IN_BYPASS = false;
+    private FirebaseAuth firebaseAuth;
+
+    private LoginMVP.View view;
+    private LoginModel model;
+
+    private SchedulerHolders schedulerHolders;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    public LoginPresenter(LoginMVP.View view, LoginModel model) {
+        this.view = view;
+        this.model = model;
+        firebaseAuth = FirebaseAuth.getInstance();
+        this.schedulerHolders = new SchedulerHolders(AndroidSchedulers.mainThread(), Schedulers.from(Executors.newFixedThreadPool(4)));
+    }
+
+    @Override
+    public void login(String login, String password) {
+
+        if(SIGN_IN_BYPASS) {
+            view.onLoginSuccessful(1L);
+        } else
+        firebaseAuth.signInWithEmailAndPassword(login, password).addOnCompleteListener(task -> {
+           if(task.isSuccessful()) {
+               compositeDisposable.add(model.getUser(task.getResult().getUser().getUid())
+               .subscribeOn(schedulerHolders.subscribe())
+               .observeOn(schedulerHolders.observ())
+               .subscribeWith(new SignInObserver()));
+           } else {
+               view.onLoginFailed();
+           }
+        });
+
+    }
+
+    private class SignInObserver extends DisposableSingleObserver<User> {
+        @Override
+        public void onSuccess(User user) {
+            view.onLoginSuccessful(user.getId());
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            view.onLoginFailed();
+        }
+    }
 }
